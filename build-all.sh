73cdebe872c17c6575455003be5321d78a67f2fd
Lars Kellogg-Stedman <lars@oddbit.com>
2026-09-13 15:28:15 -0400

    Mitigate excessive resource consumption on rasberry pi builds

--- build-all.sh
+++ build-all.sh
@@ -1,4 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-docker compose build &&
+compose_args=()
+compose_build_args=()
+
+if grep -q 'Raspberry Pi' /proc/cpuinfo; then
+  echo "Activating raspberry pi mitigations"
+  export BUILDX_BAKE_MAX_PARALLEL=1
+  export GOMAXPROCS=1
+  compose_args+=(--progress=plain)
+  compose_build_args=(--parallel=false)
+fi
+
+docker compose "${compose_args[@]}" build "${compose_build_args[@]}" &&
   docker build -t unixbbs-user -f container/user/Containerfile container/user
