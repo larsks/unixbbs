709dd4959d54a0dc7b1d8807824e9905ef4eb461
Lars Kellogg-Stedman <lars@oddbit.com>
2026-09-13 10:54:49 -0400

    Add cron service container

--- build-all.sh
+++ build-all.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 docker compose build &&
-  docker build -t unixbbs-user -f container/user/Containerfile container/user &&
-  docker build -t unixbbs-admin -f container/admin/Containerfile container/admin
+  docker build -t unixbbs-user -f container/user/Containerfile container/user
